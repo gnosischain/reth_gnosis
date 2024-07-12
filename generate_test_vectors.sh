d83f0b1b24e294a14340cfc43edd5a2ab183b206
dapplion <35266934+dapplion@users.noreply.github.com>
2024-07-12 10:23:46 +0200

    Run Reth

--- generate_test_vectors.sh
+++ generate_test_vectors.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-docker run --name neth-vec-gen --rm \
+# Clean up existing container if it exists
+docker rm -f neth-vec-gen 2>/dev/null
+
+# Start the container in the background
+docker run --name neth-vec-gen --rm -d \
   -v $PWD/networkdata:/networkdata \
+  -p 8545:8545 \
+  -p 8546:8546 \
   nethermind/nethermind \
   --config=none \
   --Init.ChainSpecPath=/networkdata/chainspec.json \
@@ -14,3 +20,19 @@ docker run --name neth-vec-gen --rm \
   --JsonRpc.EnginePort=8546 \
   --JsonRpc.JwtSecretFile=/networkdata/jwtsecret \
   --TraceStore.Enabled=true 
+
+# Capture the logs in the background
+docker logs -f neth-vec-gen &
+
+# Retry the curl command until it succeeds
+until curl -X POST -H "Content-Type: application/json" \
+  --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["0x0", false],"id":1}' \
+  http://localhost:8545; do
+    echo "Retrying..."
+    sleep 2
+done
+
+
+# Clean up container
+docker rm -f neth-vec-gen 2>/dev/null
+
---
+++ run_reth.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# reth flags from https://github.com/kurtosis-tech/ethereum-package/blob/7f365da6607bd863b12170ed475b77f4fafcc146/src/el/reth/reth_launcher.star#L206
+#
+# reth genesis from https://github.com/ethpandaops/ethereum-genesis-generator/blob/a4b6733ea9d47b2b2ec497f5212f0265b83fb601/apps/el-gen/genesis_geth.py#L34
+
+DATA_DIR=$TMPDIR/reth_test
+# Ensure no data from previous tests
+rm -rf $DATA_DIR
+
+# $PWD/target/release/reth \
+cargo run -- \
+  node \
+  --chain=$PWD/networkdata/reth_genesis.json \
+  --datadir=$DATA_DIR \
+  --http \
+  --http.port=8545 \
+  --http.addr=0.0.0.0 \
+  --http.corsdomain='*' \
+  --http.api=admin,net,eth,web3,debug,trace \
+  --authrpc.port=8546 \
+  --authrpc.jwtsecret=$PWD/networkdata/jwtsecret \
+  --authrpc.addr=0.0.0.0 \
+  --port=0 \
+  --disable-discovery
+
