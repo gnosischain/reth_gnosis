09d21bc86691a4d1a62bbd790500c637b2fb1587
debjit <debjitbhowal.db@gmail.com>
2024-10-11 20:34:32 +0530

    ci final fix minus generation

--- scripts/apply_test_vectors.sh
+++ scripts/apply_test_vectors.sh
@@ -66,6 +66,13 @@ function apply_block_file() {
     http://localhost:8546 \
   )
   echo engine_forkchoiceUpdatedV1 set new block as head RESPONSE $RESPONSE
+
+  PAYLOAD_STATUS=$(echo $RESPONSE | jq --raw-output '.result.payloadStatus.status')
+  # If the status is not "VALID", exit the script with a non-zero code to make CI fail
+  if [ "$STATUS" != "VALID" ]; then
+    echo "Error: Payload status is $STATUS, failing CI."
+    exit 1
+  fi
 }
 
 
