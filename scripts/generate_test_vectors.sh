44838b6a51e2c6c9424f6e1b878e2b353fe2b0f1
debjit <debjitbhowal.db@gmail.com>
2024-10-13 13:06:07 +0530

    final changes

--- scripts/generate_test_vectors.sh
+++ scripts/generate_test_vectors.sh
@@ -29,131 +29,130 @@ declare -i BLOCK_COUNTER=0
 
 function make_block() {
 
-# increment block counter
-BLOCK_COUNTER=$((BLOCK_COUNTER + 1))
+  # increment block counter
+  BLOCK_COUNTER=$((BLOCK_COUNTER + 1))
 
-echo "Making block $BLOCK_COUNTER"
+  echo "Making block $BLOCK_COUNTER"
 
-HEAD_BLOCK=$(curl -X POST -H "Content-Type: application/json" \
-  --data "{
-    \"jsonrpc\":\"2.0\",
-    \"method\":\"eth_getBlockByNumber\",
-    \"params\":[
-      \"latest\",
-      false
-    ],
-    \"id\":1
-  }" \
-  http://localhost:8545 \
-)
+  HEAD_BLOCK=$(curl -X POST -H "Content-Type: application/json" \
+    --data "{
+      \"jsonrpc\":\"2.0\",
+      \"method\":\"eth_getBlockByNumber\",
+      \"params\":[
+        \"latest\",
+        false
+      ],
+      \"id\":1
+    }" \
+    http://localhost:8545 \
+  )
 
-# --raw-output remove the double quotes
-HEAD_BLOCK_HASH=$(echo $HEAD_BLOCK | jq --raw-output '.result.hash')
-echo HEAD_BLOCK_HASH=$HEAD_BLOCK_HASH
+  # --raw-output remove the double quotes
+  HEAD_BLOCK_HASH=$(echo $HEAD_BLOCK | jq --raw-output '.result.hash')
+  echo HEAD_BLOCK_HASH=$HEAD_BLOCK_HASH
 
-# The ASCII representation of `2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a`
-JWT_SECRET="********************************"
-# Generate a JWT token using the secret key
-# jwt is this CLI tool https://github.com/mike-engel/jwt-cli/tree/main
-# iat is appended automatically
-JWT_TOKEN=$(jwt encode --alg HS256 --secret "$JWT_SECRET")
-echo JWT_TOKEN: $JWT_TOKEN
+  # The ASCII representation of `2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a`
+  JWT_SECRET="********************************"
+  # Generate a JWT token using the secret key
+  # jwt is this CLI tool https://github.com/mike-engel/jwt-cli/tree/main
+  # iat is appended automatically
+  JWT_TOKEN=$(jwt encode --alg HS256 --secret "$JWT_SECRET")
+  echo JWT_TOKEN: $JWT_TOKEN
 
-TIMESTAMP=$((1700000000 + BLOCK_COUNTER))
+  TIMESTAMP=$((1700000000 + BLOCK_COUNTER))
 
-# Request to produce block on current head
+  # Request to produce block on current head
 
-RESPONSE=$(curl -X POST -H "Content-Type: application/json" \
-  -H "Authorization: Bearer $JWT_TOKEN" \
-  --data "{
-    \"jsonrpc\":\"2.0\",
-    \"method\":\"engine_forkchoiceUpdatedV1\",
-    \"params\":[
-      {
-        \"headBlockHash\": \"$HEAD_BLOCK_HASH\",
-        \"safeBlockHash\": \"0x0000000000000000000000000000000000000000000000000000000000000000\",
-        \"finalizedBlockHash\": \"0x0000000000000000000000000000000000000000000000000000000000000000\"
-      },
-      {
-        \"timestamp\": $TIMESTAMP,
-        \"prevRandao\": \"0x0000000000000000000000000000000000000000000000000000000000000000\",
-        \"suggestedFeeRecipient\": \"0x0000000000000000000000000000000000000000\"
-      }
-    ],
-    \"id\":1
-  }" \
-  http://localhost:8546 \
-)
-echo engine_forkchoiceUpdatedV1 trigger block production RESPONSE $RESPONSE
+  RESPONSE=$(curl -X POST -H "Content-Type: application/json" \
+    -H "Authorization: Bearer $JWT_TOKEN" \
+    --data "{
+      \"jsonrpc\":\"2.0\",
+      \"method\":\"engine_forkchoiceUpdatedV1\",
+      \"params\":[
+        {
+          \"headBlockHash\": \"$HEAD_BLOCK_HASH\",
+          \"safeBlockHash\": \"0x0000000000000000000000000000000000000000000000000000000000000000\",
+          \"finalizedBlockHash\": \"0x0000000000000000000000000000000000000000000000000000000000000000\"
+        },
+        {
+          \"timestamp\": $TIMESTAMP,
+          \"prevRandao\": \"0x0000000000000000000000000000000000000000000000000000000000000000\",
+          \"suggestedFeeRecipient\": \"0x0000000000000000000000000000000000000000\"
+        }
+      ],
+      \"id\":1
+    }" \
+    http://localhost:8546 \
+  )
+  echo engine_forkchoiceUpdatedV1 trigger block production RESPONSE $RESPONSE
 
-PAYLOAD_ID=$(echo $RESPONSE | jq --raw-output '.result.payloadId')
-echo PAYLOAD_ID=$PAYLOAD_ID
+  PAYLOAD_ID=$(echo $RESPONSE | jq --raw-output '.result.payloadId')
+  echo PAYLOAD_ID=$PAYLOAD_ID
 
-# Fetch producing block by payload ID
+  # Fetch producing block by payload ID
 
-RESPONSE=$(curl -X POST -H "Content-Type: application/json" \
-  -H "Authorization: Bearer $JWT_TOKEN" \
-  --data "{
-    \"jsonrpc\":\"2.0\",
-    \"method\":\"engine_getPayloadV1\",
-    \"params\":[
-      \"$PAYLOAD_ID\"
-    ],
-    \"id\":1
-  }" \
-  http://localhost:8546 \
-)
-echo engine_getPayloadV1 RESPONSE $RESPONSE
+  RESPONSE=$(curl -X POST -H "Content-Type: application/json" \
+    -H "Authorization: Bearer $JWT_TOKEN" \
+    --data "{
+      \"jsonrpc\":\"2.0\",
+      \"method\":\"engine_getPayloadV1\",
+      \"params\":[
+        \"$PAYLOAD_ID\"
+      ],
+      \"id\":1
+    }" \
+    http://localhost:8546 \
+  )
+  echo engine_getPayloadV1 RESPONSE $RESPONSE
 
-BLOCK=$(echo $RESPONSE | jq '.result')
-# BLOCK_NUMBER_HEX = 0x1, 0x2, etc
-BLOCK_NUMBER_HEX_PREFIX=$(echo $BLOCK | jq --raw-output '.blockNumber')
-BLOCK_NUMBER_HEX=${BLOCK_NUMBER_HEX_PREFIX#"0x"}
-BLOCK_NUMBER=$((16#$BLOCK_NUMBER_HEX))
-BLOCK_HASH=$(echo $BLOCK | jq --raw-output '.blockHash')
+  BLOCK=$(echo $RESPONSE | jq '.result')
+  # BLOCK_NUMBER_HEX = 0x1, 0x2, etc
+  BLOCK_NUMBER_HEX_PREFIX=$(echo $BLOCK | jq --raw-output '.blockNumber')
+  BLOCK_NUMBER_HEX=${BLOCK_NUMBER_HEX_PREFIX#"0x"}
+  BLOCK_NUMBER=$((16#$BLOCK_NUMBER_HEX))
+  BLOCK_HASH=$(echo $BLOCK | jq --raw-output '.blockHash')
 
-# persist the block as test-vector
+  # persist the block as test-vector
 
-echo $BLOCK | jq '.' > $OUT_DIR/block_$BLOCK_NUMBER_HEX.json
+  echo $BLOCK | jq '.' > $OUT_DIR/block_$BLOCK_NUMBER_HEX.json
 
-# send the new block as payload
+  # send the new block as payload
 
-RESPONSE=$(curl -X POST -H "Content-Type: application/json" \
-  -H "Authorization: Bearer $JWT_TOKEN" \
-  --data "{
-    \"jsonrpc\":\"2.0\",
-    \"method\":\"engine_newPayloadV1\",
-    \"params\":[
-      $BLOCK
-    ],
-    \"id\":1
-  }" \
-  http://localhost:8546 \
-)
-echo engine_newPayloadV1 with new block RESPONSE $RESPONSE
+  RESPONSE=$(curl -X POST -H "Content-Type: application/json" \
+    -H "Authorization: Bearer $JWT_TOKEN" \
+    --data "{
+      \"jsonrpc\":\"2.0\",
+      \"method\":\"engine_newPayloadV1\",
+      \"params\":[
+        $BLOCK
+      ],
+      \"id\":1
+    }" \
+    http://localhost:8546 \
+  )
+  echo engine_newPayloadV1 with new block RESPONSE $RESPONSE
 
 
-# set the block as head
+  # set the block as head
 
-RESPONSE=$(curl -X POST -H "Content-Type: application/json" \
-  -H "Authorization: Bearer $JWT_TOKEN" \
-  --data "{
-    \"jsonrpc\":\"2.0\",
-    \"method\":\"engine_forkchoiceUpdatedV1\",
-    \"params\":[
-      {
-        \"headBlockHash\": \"$BLOCK_HASH\",
-        \"safeBlockHash\": \"0x0000000000000000000000000000000000000000000000000000000000000000\",
-        \"finalizedBlockHash\": \"0x0000000000000000000000000000000000000000000000000000000000000000\"
-      },
-      null
-    ],
-    \"id\":1
-  }" \
-  http://localhost:8546 \
-)
-echo engine_forkchoiceUpdatedV1 set new block as head RESPONSE $RESPONSE
-
+  RESPONSE=$(curl -X POST -H "Content-Type: application/json" \
+    -H "Authorization: Bearer $JWT_TOKEN" \
+    --data "{
+      \"jsonrpc\":\"2.0\",
+      \"method\":\"engine_forkchoiceUpdatedV1\",
+      \"params\":[
+        {
+          \"headBlockHash\": \"$BLOCK_HASH\",
+          \"safeBlockHash\": \"0x0000000000000000000000000000000000000000000000000000000000000000\",
+          \"finalizedBlockHash\": \"0x0000000000000000000000000000000000000000000000000000000000000000\"
+        },
+        null
+      ],
+      \"id\":1
+    }" \
+    http://localhost:8546 \
+  )
+  echo engine_forkchoiceUpdatedV1 set new block as head RESPONSE $RESPONSE
 }
 
 # Number of times to call make_block
