eccd9c5166da3d839ed4412918558cd67cc572fc
debjit <debjitbhowal.db@gmail.com>
2024-12-03 18:30:06 +0530

    testing setup

---
+++ src/testing/mod.rs
@@ -0,0 +1,17 @@
+#![allow(dead_code)]
+#![cfg(test)]
+#[cfg(feature = "testing")]
+
+pub mod case;
+pub mod result;
+pub mod suite;
+
+pub mod assert;
+pub mod cases;
+pub mod models;
+
+pub use case::Case;
+pub use result::Error;
+pub use suite::Suite;
+
+pub mod tests;
