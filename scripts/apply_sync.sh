a2187f849e19f36d45e73983bcad90a6d55da2eb
debjit <debjitbhowal.db@gmail.com>
2024-12-16 12:59:24 +0530

    made block application faster

--- scripts/apply_sync.sh
+++ scripts/apply_sync.sh
@@ -8,12 +8,14 @@ set -e
 # Script's directory
 DIR="$(dirname "$0")"
 
-BLOCKS_FOLDER=${1:-syncblocks}
-OUT_DIR=$DIR/$BLOCKS_FOLDER
+# BLOCKS_FOLDER=${1:-syncblocks}
+# BLOCKS_FOLDER=${1:-syncblocks}
+# OUT_DIR=$DIR/$BLOCKS_FOLDER
+OUT_DIR=/Users/debjit/Drawer/gnosis/rlp/blocks
 
-N=${2:-1}
+N=${1:-1}
 
-START_BLOCK_NUMBER=26478704
+START_BLOCK_NUMBER=26539985
 
 # Retry the curl command until it succeeds
 until curl -X POST -H "Content-Type: application/json" \
@@ -25,10 +27,13 @@ done
 
 echo "\n\nEL is available\n\n"
 
+FINALIZATION_INTERVAL=1
+CURRENT_COUNT=1
+
 function apply_block_file() {
   BLOCK_FILEPATH=$1
   BLOCK=$(<$BLOCK_FILEPATH)
-  echo Applying $BLOCK "\n\n"
+  # echo Applying $BLOCK "\n\n"
 
   # The ASCII representation of `2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a`
   JWT_SECRET="********************************"
@@ -36,7 +41,7 @@ function apply_block_file() {
   # jwt is this CLI tool https://github.com/mike-engel/jwt-cli/tree/main
   # iat is appended automatically
   JWT_TOKEN=$(jwt encode --alg HS256 --secret "$JWT_SECRET")
-  echo JWT_TOKEN: $JWT_TOKEN
+  # echo JWT_TOKEN: $JWT_TOKEN
 
   # RESPONSE=$(curl -X POST -H "Content-Type: application/json" \
   #   -H "Authorization: Bearer $JWT_TOKEN" \
@@ -70,37 +75,44 @@ function apply_block_file() {
     }" \
     http://localhost:8546 \
   )
-  echo engine_newPayloadV1 with new block RESPONSE $RESPONSE
+  # echo engine_newPayloadV1 with new block RESPONSE $RESPONSE
 
   BLOCK_HASH=$(echo $BLOCK | jq --raw-output '.blockHash')
 
-  RESPONSE=$(curl -X POST -H "Content-Type: application/json" \
-    -H "Authorization: Bearer $JWT_TOKEN" \
-    --data "{
-      \"jsonrpc\":\"2.0\",
-      \"method\":\"engine_forkchoiceUpdatedV1\",
-      \"params\":[
-        {
-          \"headBlockHash\": \"$BLOCK_HASH\",
-          \"safeBlockHash\": \"0x0000000000000000000000000000000000000000000000000000000000000000\",
-          \"finalizedBlockHash\": \"0x0000000000000000000000000000000000000000000000000000000000000000\"
-        },
-        null
-      ],
-      \"id\":1
-    }" \
-    http://localhost:8546 \
-  )
-  echo engine_forkchoiceUpdatedV1 set new block as head RESPONSE $RESPONSE
+  if [ $((CURRENT_COUNT % FINALIZATION_INTERVAL)) -eq 0 ]; then
+    echo "Finalizing block $BLOCK_HASH"
 
+    RESPONSE=$(curl -X POST -H "Content-Type: application/json" \
+      -H "Authorization: Bearer $JWT_TOKEN" \
+      --data "{
+        \"jsonrpc\":\"2.0\",
+        \"method\":\"engine_forkchoiceUpdatedV1\",
+        \"params\":[
+          {
+            \"headBlockHash\": \"$BLOCK_HASH\",
+            \"safeBlockHash\": \"0x0000000000000000000000000000000000000000000000000000000000000000\",
+            \"finalizedBlockHash\": \"0x0000000000000000000000000000000000000000000000000000000000000000\"
+          },
+          null
+        ],
+        \"id\":1
+      }" \
+      http://localhost:8546 \
+    )
+    echo engine_forkchoiceUpdatedV1 set new block as head RESPONSE $RESPONSE
 
-  PAYLOAD_STATUS=$(echo $RESPONSE | jq --raw-output '.result.payloadStatus.status')
-  echo PAYLOAD_STATUS: $PAYLOAD_STATUS
-  # If the status is not "VALID", exit the script with a non-zero code to make CI fail
-  if [ "$PAYLOAD_STATUS" != "VALID" ]; then
-    echo "Error: Payload status is $PAYLOAD_STATUS, failing CI."
-    exit 1
+    PAYLOAD_STATUS=$(echo $RESPONSE | jq --raw-output '.result.payloadStatus.status')
+    echo PAYLOAD_STATUS: $PAYLOAD_STATUS
+    # If the status is not "VALID", exit the script with a non-zero code to make CI fail
+    if [ "$PAYLOAD_STATUS" != "VALID" ]; then
+      echo "Error: Payload status is $PAYLOAD_STATUS, failing CI."
+      exit 1
+    fi
+    
+    CURRENT_COUNT=1
   fi
+
+  CURRENT_COUNT=$((CURRENT_COUNT + 1))
 }
 
 # start from START_BLOCK_NUMBER
