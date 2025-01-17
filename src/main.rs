3dcec7225082135992b46e90721057c43d1ca79c
debjit <debjitbhowal.db@gmail.com>
2025-01-17 14:04:05 +0530

    cli

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,5 @@
 use clap::{Args, Parser};
-use reth::{
-    chainspec::EthereumChainSpecParser,
-    cli::{Cli, Commands},
-    CliRunner,
-};
-use reth_gnosis::{
-    execute::{GnosisExecutionStrategyFactory, GnosisExecutorProvider},
-    GnosisNode,
-};
-use reth_node_ethereum::BasicBlockExecutorProvider;
-use tracing::{error, info};
+use reth_gnosis::{cli::Cli, spec::GnosisChainSpecParser, GnosisNode};
 
 // We use jemalloc for performance reasons
 #[cfg(all(feature = "jemalloc", unix))]
@@ -29,40 +19,12 @@ fn main() {
         std::env::set_var("RUST_BACKTRACE", "1");
     }
 
-    let cli = Cli::<EthereumChainSpecParser, NoArgs>::parse();
-    let _ = cli.init_tracing().unwrap();
+    if let Err(err) = Cli::<GnosisChainSpecParser, NoArgs>::parse().run(|builder, _| async move {
+        let handle = builder.node(GnosisNode::new()).launch().await?;
 
-    match cli.command {
-        Commands::Import(command) => {
-            info!(target: "reth::cli", "Importing with custom cli");
-            let runner = CliRunner::default();
-            let res = runner.run_blocking_until_ctrl_c(command.execute::<GnosisNode, _, _>(
-                |chain_spec| -> BasicBlockExecutorProvider<GnosisExecutionStrategyFactory> {
-                    GnosisExecutorProvider::gnosis(chain_spec)
-                },
-            ));
-            if let Err(err) = res {
-                error!(target: "reth::cli", "Error: {err:?}");
-                std::process::exit(1);
-            }
-        }
-        Commands::InitState(command) => {
-            let runner = CliRunner::default();
-            let res = runner.run_blocking_until_ctrl_c(command.execute::<GnosisNode>());
-            if let Err(err) = res {
-                error!(target: "reth::cli", "Error: {err:?}");
-                std::process::exit(1);
-            }
-        }
-        _ => {
-            if let Err(err) = cli.run(|builder, _| async move {
-                let handle = builder.node(GnosisNode::new()).launch().await?;
-
-                handle.node_exit_future.await
-            }) {
-                error!(target: "reth::cli", "Error: {err:?}");
-                std::process::exit(1);
-            }
-        }
+        handle.node_exit_future.await
+    }) {
+        eprintln!("Error: {err:?}");
+        std::process::exit(1);
     }
 }
