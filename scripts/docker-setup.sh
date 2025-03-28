8c749194379a8880595e3e0bb066aff5727dbffd
debjit <debjitbhowal.db@gmail.com>
2025-03-28 16:13:15 +0530

    docker stuff

--- scripts/docker-setup.sh
+++ scripts/docker-setup.sh
@@ -38,13 +38,12 @@ fi
 # if import success file is missing, import the state
 if [ ! -f $IMPORT_SUCCESS_FILE ] || [ $CLEAR_FLAG = true ]; then
     echo "Dropping existing database..."
-    ./target/debug/reth --chain ./scripts/chainspecs/chiado.json db drop -f || true
+    docker run -v $DATA_DIR:/data reth --chain chainspecs/chiado.json db --datadir /data drop -f || true
 
     echo "Importing the state"
-    ./target/debug/reth --chain ./scripts/chainspecs/chiado.json init-state $STATE_FILE --without-evm --header $HEADER_FILE --total-difficulty 231708131825107706987652208063906496124457284 --header-hash 08cf5eed684e84eccb9809d1d8de287b0bfad27e735c60e98709ab060106b04c
+    docker run -v $DATA_DIR:/data reth --chain chainspecs/chiado.json init-state /data/state_at_700000.jsonl --without-evm --header /data/header_700000.rlp --total-difficulty 231708131825107706987652208063906496124457284 --header-hash 08cf5eed684e84eccb9809d1d8de287b0bfad27e735c60e98709ab060106b04c --datadir /data
     touch $IMPORT_SUCCESS_FILE
 fi
 
 echo "2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a2a" > $DATA_DIR/jwtsecret
-
 echo -e "\033[0;32mSetup complete\033[0m"
