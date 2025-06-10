d6f88f0cfabedb1e4e3654fcd98b4b406fc943a4
Debjit <github@debjit.dev>
2025-06-11 01:04:27 +0530

    upgrade

--- src/block.rs
+++ src/block.rs
@@ -118,14 +118,17 @@ where
         Ok(())
     }
 
-    fn execute_transaction_with_result_closure(
+    fn execute_transaction_with_commit_condition(
         &mut self,
         tx: impl ExecutableTx<Self>,
-        f: impl FnOnce(&ExecutionResult<<Self::Evm as Evm>::HaltReason>),
-    ) -> Result<u64, BlockExecutionError> {
+        f: impl FnOnce(
+            &ExecutionResult<<Self::Evm as Evm>::HaltReason>,
+        ) -> reth_evm::block::CommitChanges,
+    ) -> Result<Option<u64>, BlockExecutionError> {
         // The sum of the transaction's gas limit, Tg, and the gas utilized in this block prior,
         // must be no greater than the block's gasLimit.
         let block_available_gas = self.evm.block().gas_limit - self.gas_used;
+
         if tx.tx().gas_limit() > block_available_gas {
             return Err(
                 BlockValidationError::TransactionGasLimitMoreThanAvailableBlockGas {
@@ -137,17 +140,17 @@ where
         }
 
         // Execute transaction.
-        let result_and_state = self
+        let ResultAndState { result, state } = self
             .evm
             .transact(tx)
             .map_err(|err| BlockExecutionError::evm(err, tx.tx().trie_hash()))?;
-        self.system_caller.on_state(
-            StateChangeSource::Transaction(self.receipts.len()),
-            &result_and_state.state,
-        );
-        let ResultAndState { result, state } = result_and_state;
 
-        f(&result);
+        if !f(&result).should_commit() {
+            return Ok(None);
+        }
+
+        self.system_caller
+            .on_state(StateChangeSource::Transaction(self.receipts.len()), &state);
 
         let gas_used = result.gas_used();
 
@@ -167,7 +170,7 @@ where
         // Commit the state changes.
         self.evm.db_mut().commit(state);
 
-        Ok(gas_used)
+        Ok(Some(gas_used))
     }
 
     fn finish(
