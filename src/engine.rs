99e963a2c98e94bc0eedfb013a71fbdad48aa941
Debjit Bhowal <debjitbhowal.db@gmail.com>
2026-03-09 12:26:28 +0530

    chore: remove unused codepaths (#112)

--- src/engine.rs
+++ src/engine.rs
@@ -3,15 +3,10 @@
 // Needed for AddOns, debug capabilities and custom primitives
 
 use crate::{
-    consts::{
-        is_blacklisted_setcode, is_sender_blacklisted, is_to_address_blacklisted, GnosisError,
-        DEFAULT_7702_PATCH_TIME, DEFAULT_EL_PATCH_TIME,
-    },
     payload::GnosisBuiltPayload,
     primitives::block::{GnosisBlock, IntoGnosisBlock, TransactionSigned},
-    spec::gnosis_spec::{GnosisChainSpec, GnosisHardForks},
+    spec::gnosis_spec::GnosisChainSpec,
 };
-use alloy_consensus::Transaction;
 use reth::rpc::types::engine::{ExecutionData, ExecutionPayload, ExecutionPayloadEnvelopeV5};
 use reth_ethereum_engine_primitives::{
     EthPayloadAttributes, ExecutionPayloadEnvelopeV2, ExecutionPayloadEnvelopeV3,
@@ -27,7 +22,7 @@ use reth_payload_builder::EthPayloadBuilderAttributes;
 use reth_primitives::{NodePrimitives, RecoveredBlock};
 use reth_primitives_traits::SealedBlock;
 use serde::{Deserialize, Serialize};
-use std::{env, sync::Arc};
+use std::sync::Arc;
 
 /// Custom engine types - uses a custom payload attributes RPC type, but uses the default
 /// payload builder attributes type.
@@ -95,37 +90,6 @@ impl PayloadValidator<GnosisEngineTypes> for GnosisEngineValidator {
         let block = sealed_block
             .try_recover()
             .map_err(|e| NewPayloadError::Other(e.into()))?;
-
-        if !self
-            .chain_spec()
-            .is_balancer_hardfork_active_at_timestamp(block.timestamp)
-            && block.timestamp
-                > env::var("GNOSIS_EL_PATCH_TIME")
-                    .unwrap_or(DEFAULT_EL_PATCH_TIME.to_string())
-                    .parse::<u64>()
-                    .unwrap_or_default()
-        {
-            let is_patch2_enabled: bool = block.timestamp
-                > env::var("GNOSIS_EL_7702_PATCH_TIME")
-                    .unwrap_or(DEFAULT_7702_PATCH_TIME.to_string())
-                    .parse::<u64>()
-                    .unwrap_or_default();
-
-            for (sender, tx) in block.transactions_with_sender() {
-                if is_sender_blacklisted(sender)
-                    || is_to_address_blacklisted(&tx.to().unwrap_or_default())
-                    || (is_patch2_enabled && is_blacklisted_setcode(tx))
-                {
-                    return Err(NewPayloadError::other(GnosisError::custom(format!(
-                            "Unable to proceed (ensure_well_formed_payload) - signer: {}, to: {:?}, block: {}, {}",
-                            &sender,
-                            &tx.to().unwrap_or_default(),
-                            &block.number,
-                            &block.hash()
-                        ))));
-                }
-            }
-        }
 
         Ok(block)
     }
