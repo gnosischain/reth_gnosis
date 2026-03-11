7f034df06b7d8dcbb66a867f3d0896f5f859504b
Philippe Schommers <philippe@schommers.be>
2026-03-11 17:56:44 +0100

    feat: add flashbots block validation RPC

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
 use clap::{Args, Parser};
+use reth::api::FullNodeComponents;
 use reth_cli_commands::common::EnvironmentArgs;
 use reth_gnosis::cli::gnosis_cli::Commands;
+use reth_gnosis::engine::GnosisEngineValidator;
 use reth_gnosis::initialize::download_init_state::{CHIADO_DOWNLOAD_SPEC, GNOSIS_DOWNLOAD_SPEC};
 use reth_gnosis::initialize::import_and_ensure_state::download_and_import_init_state;
 use reth_gnosis::{
     cli::gnosis_cli::GnosisCli, spec::gnosis_spec::GnosisChainSpecParser, GnosisNode,
 };
+use reth_rpc::ValidationApi;
+use reth_rpc_api::servers::BlockSubmissionValidationApiServer;
+use reth_rpc_builder::{config::RethRpcServerConfig, RethRpcModule};
+use std::sync::Arc;
 
 // We use jemalloc for performance reasons
 #[cfg(all(feature = "jemalloc", unix))]
@@ -48,6 +54,21 @@ fn run_reth(cli: CliGnosis) {
     if let Err(err) = cli.run(|builder, _| async move {
         let handle = builder
             .node(GnosisNode::new())
+            .extend_rpc_modules(|ctx| {
+                let validation_api = ValidationApi::new(
+                    ctx.provider().clone(),
+                    Arc::new(ctx.node().consensus().clone()),
+                    ctx.node().evm_config().clone(),
+                    ctx.config().rpc.flashbots_config(),
+                    Box::new(ctx.node().task_executor().clone()),
+                    Arc::new(GnosisEngineValidator::new(ctx.config().chain.clone())),
+                );
+                ctx.modules.merge_if_module_configured(
+                    RethRpcModule::Flashbots,
+                    validation_api.into_rpc(),
+                )?;
+                Ok(())
+            })
             .launch_with_debug_capabilities()
             .await?;
 
